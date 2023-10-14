825788c86abac427bfdf3b26b4ccc5533b511915
Ekrcoaster <32022835+Ekrcoaster@users.noreply.github.com>
2023-10-14 11:29:33 -0700

    oop, bug fix

--- assets/builtInComponents/_builtInComponents.js
+++ assets/builtInComponents/_builtInComponents.js
@@ -1,3 +1,3 @@
 const BUILD_IN_COMPONENTS = {
-    "Rotator": " class Rotator extends Component { [Number] degPerSecond = 90; onUpdate() { this.gameObject.transform.localAngle += engine.deltaTime * this.amt; } }"
+    "Rotator": " class Rotator extends Component { [Number] degPerSecond = 90; onUpdate() { this.gameObject.transform.localAngle += engine.deltaTime * this.degPerSecond; } }"
 }
